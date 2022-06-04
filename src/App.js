c06d7ecfcc93b7bc63da3de7ede6ce9012756ad5
ammar <ammarraneez@gmail.com>
2022-06-04 11:04:37 +0530

    feat: add global style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import Game from './pages/Game';
 import GameOver from './pages/GameOver';
 import HighScores from './pages/HighScores';
 import Home from './pages/Home';
+import { Container } from './styled/Container';
+import Global from './styled/Global';
+import { Main } from './styled/Main';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/game" element={<Game />} />
-        <Route path="/highScores" element={<HighScores />} />
-        <Route path="/gameOver" element={<GameOver />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Routes>
+            <Route path="/game" element={<Game />} />
+            <Route path="/highScores" element={<HighScores />} />
+            <Route path="/gameOver" element={<GameOver />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </Main>
     </Router>
   );
 }
