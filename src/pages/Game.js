982a42341d9a6fa7fae56c15aa7b783dc639acf4
ammar <ammarraneez@gmail.com>
2022-06-04 11:50:33 +0530

    Style game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {
+  StyledCharacter,
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+} from '../styled/Game';
+import { Strong } from '../styled/Shared';
 
 function Game() {
   return (
-    <div>Game</div>
+    <StyledGame>
+      <StyledScore>
+        Score: <Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <Strong>00:000</Strong>
+      </StyledTimer>
+    </StyledGame>
   );
 }
 
