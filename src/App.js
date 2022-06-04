d5065b90fab6fad7b8357bef8b0d55d69d5bb88b
ammar <ammarraneez@gmail.com>
2022-06-04 09:26:27 +0530

    Base setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
+import GameOver from './pages/GameOver';
+import HighScores from './pages/HighScores';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/game" element={<Game />} />
+        <Route path="/highScores" element={<HighScores />} />
+        <Route path="/gameOver" element={<GameOver />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
