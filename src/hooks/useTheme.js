a110769bee83e66b416c698dd39af92bb8298829
ammar <ammarraneez@gmail.com>
2022-06-16 08:48:04 +0530

    feat: persist theme in localstorage

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useTheme = () => {
   const [theme, setTheme] = useState('light');
 
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme');
+    setTheme(localTheme || 'light');
+  }, []);
+
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       setTheme('light');
+      localStorage.setItem('theme', 'light');
     }
   };
 
